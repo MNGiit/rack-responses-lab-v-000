25491f828e5c1ff05de1d0b65b5f46e6981aac77
Mohammed Noori <kmanoorib@gmail.com>
2018-11-13 00:57:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,35 +1,15 @@
 require 'pry'
 class Application
-  
+
   def call(env)
     resp = Rack::Response.new
-    
-    time = Time.new
-    
-    wrote_greetings = false
-    
-    if time.hour >=12
-      resp.write "Good Afternoon!"
+    now = Time.now
+    if now.hour >=12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
     end
-    
-    
-    
-    #if time.hour < 12
-    #  resp.write "Good Morning!"
-    #  wrote_greetings = true
-    #elsif time.hour >=12 
-    #  resp.write "Good Afternoon!"
-    #end
-    
-    #resp.write "Good Afternoon!" if wrote_greetings == false
-    
-    #if time.hour < 12
-    #  resp.write "Good Morning!"
-    #elsif time.hour == 12
-    #  resp.write "Good Afternoon!"
-    #end
-    
     resp.finish
   end
-  
+
 end
